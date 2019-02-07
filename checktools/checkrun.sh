01cb312de1dc8d53e42b697c61cc1bc669a11ae7
Tobias Wicky <twicki@ethz.ch>
2019-02-07 14:47:43 +0100

    integrate checktools

---
+++ checktools/checkrun.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# COSMO CHECK script
+#
+# This script run severals check (see testsuite checkers for more info)
+# It is expected to be called from the COSMO run directory using single precision executable
+# The script prints TEST RESULT : OK and return 0 or TEST RESULT : FAIL and return 1
+
+# Author       Xavier Lapillonne
+# Date         26.06.2015
+# Mail         xavier.lapillonne@meteoswiss.ch
+
+# Set variables
+checktool_dir=$(dirname $(realpath -s $0))
+status=0
+
+# set environment variables for the checkers
+export TS_RUNDIR="."
+export TS_LOGFILE="lm_f90.out"
+export TS_LOGFILE_SLURM="exe.log"
+export TS_VERBOSE=1
+export TS_REFOUTDIR="./reference_${REAL_TYPE}"
+export TS_NAMELISTDIR="."
+export TS_TOLERANCE="TOLERANCE_${REAL_TYPE}"
+export TS_TIMINGS="TIMINGS_${REAL_TYPE}.cfg"
+export TS_BASEDIR="."
+export TS_FORCEMATCH="TRUE"
+
+
+
+#run checkers
+${checktool_dir}/run_success_check.py
+if [ $? -gt 15 ]; then
+    echo "run_success_check : fail"
+    status=1
+fi
+
+# ${checktool_dir}/output_tolerance_check.py
+# if [ $? -gt 15 ]; then
+#    echo "output_tolerance_check : fail"
+#    status=1
+# fi
+
+${checktool_dir}/timing_check.py -f $1
+if [ $? -gt 15 ]; then
+    echo "timing_check : fail"
+    status=1
+fi
+
+
+if [ ${status} == 0 ]; then
+    echo TEST RESULT: OK
+else
+    echo TEST RESULT: FAIL
+fi
+
+# Check if there are changes with respect to owm YUSPECIF
+# This will send a warning if not (but won't change the exit status)
+#${checktool_dir}/yuspecif_owm_check.sh
+
+exit $status
+
+
+
+
---
+++ checktools/timing_check.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python2
+
+"""
+COSMO TECHNICAL TESTSUITE
+
+This script checks whether two runs are bit-identical by comparing the
+YUPRTEST files.
+"""
+
+# built-in modules
+import os, sys, re, json
+import argparse
+
+# private modules
+# sys.path.append('./tools') 
+sys.path.append('../checktools/tools') 
+sys.path.append('../perf-benchmarks/checktools/tools') 
+from ts_utilities import read_environ, dir_path
+from ts_fortran_nl import get_param
+from parseyu import COSMO_Run_yu
+import comp_yuprtest
+
+# some global definitions
+yufile = 'YUPRTEST'     # name of special testsuite output
+yuswitch = 'ltestsuite' # namelist switch controlling YUPRDBG output
+nlfile1 = 'INPUT_DIA'    # namelist file containing yuswitch
+nlfile2 = 'INPUT_ORG'    # namelist file containing dt
+
+
+def dir_path(path):
+    """decorate a path with a trailing slash if not present"""
+    pattern='^(.*)[/]$'
+    matchobj=re.match(pattern,path)
+    if matchobj:
+        return path
+    else:
+        return path+'/'
+
+
+def parse():
+    env = read_environ()
+    rundir = env['RUNDIR']
+    yutimings = "YUTIMING"
+    cosmolog = env['LOGFILE']
+    slurmlog = env['LOGFILE_SLURM']
+    # rundir = "."
+    # yutimings = "YUTIMING"
+    # cosmolog = "lmf_f90.log"
+    # slurmlog = "slurm.log"
+    name = "Cosmo run in "+rundir
+    return COSMO_Run_yu(folder=rundir, name=name, yutimings=yutimings, cosmolog=cosmolog, slurmlog=slurmlog)
+
+
+def get_reference_timings():
+    import ConfigParser
+    config = ConfigParser.RawConfigParser()
+    # env = read_environ()
+    # rundir = env['RUNDIR']
+    # timings_file = env['TIMINGS']
+    rundir = "."
+    timings_file = "ref_timing"
+    file_path = dir_path(rundir)+timings_file
+    config.read(file_path)
+    threshold = float(config.get('timings', 'threshold'))
+    timings = {}
+    for name, value in config.items("timings"):
+        if name == "threshold":
+            continue
+        timings[name] = float(value)
+    return (timings, threshold)
+
+
+def check_value(reference, value, threshold=0):
+    if value < (reference*(1.0+threshold/100.0)):
+        return True
+    return False
+
+
+def check(data):
+    timings, threshold = get_reference_timings()
+    status = 0
+    for name, timing_ref in timings.iteritems():
+        data_timing = data[name]
+        if data_timing is None:
+            print("Fail: No timing data available for "+name)
+        if not check_value(timing_ref, data_timing, threshold=threshold):
+            print("Fail: Could not validate "+name+": timing "+str(data_timing)+" (reference timing: "+str(timing_ref)+" with "+str(threshold)+"% threshold)")
+            status = 20
+        else:
+            print(name+": "+str(data_timing)+"s below reference: "+str(timing_ref)+"s with "+str(threshold)+"% threshold")
+    return status
+
+def checkjson(data):
+
+    timings, threshold = get_reference_timings()
+    status = 0
+    dict_data = {}
+    for data_point in data:
+        dict_data.update({data_point['stencil']: data_point['measurements'][0]})
+    for name, timing_ref in timings.iteritems():
+        data_timing = dict_data[name]
+        if data is None:
+            print("Fail: No timing data available for "+name)
+        if not check_value(timing_ref, data_timing, threshold=threshold):
+            print("Fail: Could not validate "+name+": timing "+str(data_timing)+" (reference timing: "+str(timing_ref)+" with "+str(threshold)+"% threshold)")
+            status = 20
+        else:
+            print(name+": "+str(data_timing)+"s below reference: "+str(timing_ref)+"s with "+str(threshold)+"% threshold")
+    return status
+
+
+def parse_and_write(filename="None"):
+    # Parse the logfile and the YUTIMINGS file to get all the relevant data
+    data = parse()
+
+    # Convert the format into what we need for the perf-benchmarks
+    run2 = {'config': {}, 'runtime': {}, 'times': [], 'metadata': {}}
+    for k, v in data.timings:
+        run2['times'].append({"measurements": [v], "stencil": k})
+
+    # Convert between cosmo keys and perftool keys
+    runtime_info = run2['runtime']
+    runtime_info.update({'datetime': data.metadata['Current start time']})
+    runtime_info.update({'name': data.metadata['Binary name']})
+    runtime_info.update({'version': data.metadata['Tag name']})
+
+    # Addition of all the extra information int metadata
+    def without_keys(d, keys):
+        return {k: v for k, v in d.items() if k not in keys}
+    used_keys = {'Current start time', 'Binary name', 'Tag name'}
+    for k, v in without_keys(data.metadata, used_keys).iteritems():
+        run2['metadata'][k] = v
+    # Write the json-file
+    if filename == "None":
+        from datetime import datetime
+        date = datetime.strptime(data.metadata['Current start time'], '%Y-%m-%d %H:%M')
+        filename = str(date.year) + "-" + str(date.month) + "-" + str(date.day) + "-" + str(date.hour) + "-" + \
+                   str(date.minute) + "-" + str(date.second) + ".json"
+
+    with open(filename, 'w') as outfile:
+        json.dump(run2, outfile, indent=2, sort_keys=True)
+
+
+def check_tolerance(filename="None"):
+    if filename == "unknown":
+        print "None"
+    else:
+        with open(filename) as f:
+            data = json.load(f)
+        timing = data['times']
+        checkjson(timing)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filename', '-f', type=str,
+                                     help='filename of the output json file', dest='filename')
+    args = parser.parse_args()
+    print(args.filename)
+    #parse the inputfile and write to the passed arugment
+    parse_and_write(args.filename)
+    # parse_and_write()
+
+    # check timings form the tolerance file and abort if it does not pass
+    status = check_tolerance(args.filename)
+    if status == 20:
+        sys.exit(status)
+
+
+
+
